b7e93bfdb59cfa7bb999c77c901155e846512a84
gmenez <73724162+germanjimenezz18@users.noreply.github.com>
2025-01-19 22:50:09 +0100

    faet: Schema adjust

--- src/validators/schemas.ts
+++ src/validators/schemas.ts
@@ -3,5 +3,5 @@ export const contactFormSchema = z.object({
 	name: z.string(),
 	phone: z.string().min(9).max(9).nullable(),
 	email: z.string().email(),
-	message: z.string().min(10).max(1000).nullable(),
+	message: z.string().max(1000).nullable(),
 });
